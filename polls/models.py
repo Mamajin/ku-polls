72dbe8f6e909f24ab41688aac684ca198d1fd14c
Phasit Ruangmak <phasit2005@gmail.com>
2024-09-01 17:18:56 +0700

    Question Class Feature Update

--- polls/models.py
+++ polls/models.py
@@ -9,7 +9,8 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', default=timezone.now)
+    end_date = models.DateTimeField('end date', null=True, blank=True)
 
     def __str__(self):
         return self.question_text
@@ -17,6 +18,25 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        """
+        Return True if current date/time is on or after question's
+        publication date.
+        """
+        return timezone.localtime() >= self.pub_date
+
+    def can_vote(self):
+        """
+        Returns True if voting is allowed for this question.
+        Voting is allowed if the current date/time is between pub_date
+        and end_date. If end_date is None, voting is allowed anytime after
+        pub_date.
+        """
+        now = timezone.localtime()
+        if self.end_date:
+            return self.pub_date <= now <= self.end_date
+        return now >= self.pub_date
 
 
 class Choice(models.Model):
