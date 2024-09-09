8fee3b344b2510d1b6225dd9fa34e2391abe5bb1
Phasit Ruangmak <phasit2005@gmail.com>
2024-09-09 10:48:42 +0700

    Updated poll questions and fixed poll index

--- polls/views.py
+++ polls/views.py
@@ -27,9 +27,7 @@ class IndexView(generic.ListView):
         published in the future).
         """
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
-            "-pub_date")[
-               :5
-               ]
+            "-pub_date")
 
 
 class DetailView(generic.DetailView):
